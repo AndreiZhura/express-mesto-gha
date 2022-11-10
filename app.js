f4c792a9a2f2c7087eb66a4e8751bfb15f95c339
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-11-10 16:09:14 +0300

    1

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const path = require('path');
 const express = require('express');
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
 
+const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mynewdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log('Ссылка на сервер');
+  console.log(BASE_PATH);
+});
