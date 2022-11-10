c16da7d998b7919280d6cabaa68acaf4b9f3bbc7
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-11-10 16:50:08 +0300

    переместил app

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
 
-const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mynewdb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+app.get('/', (req, res) => {
+  res.status(404).send('<h1>Страница не найдена</h1>');
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => {
-  console.log('Ссылка на сервер');
-  console.log(BASE_PATH);
-});
+// подключаемся к серверу mongo
+// подключаем мидлвары, роуты и всё остальное...
+
+app.listen(3010);
