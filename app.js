df8469b7afd34d1ea88acbb3988c9a317cee6e52
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-11-11 15:23:05 +0300

    без ошибок

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routers/users');
+const bodyParser = require('body-parser');
+const express = require('express');
+const users = require('./models/user');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use('/users', router);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/users', (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  users
+    .create({ name, about, avatar })
+    .then((data) => res.status(201).send(data))
+    .catch((err) => res.status(400).send(err));
+});
 
 app.listen(PORT);
