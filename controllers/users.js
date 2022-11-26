17eff7f623a079e9d37387e0db5b6d02008371ac
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-11-26 11:55:58 +0300

    1

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,10 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const users = require('../models/users');
 
 const {
   ERROR_CODE,
   INTERNAL_SERVER_ERROR,
   FILE_NOT_FOUND,
-  AUTHORIZATION_REQUIRED,
   SALT_ROUND,
 } = require('../constants/constants');
 
@@ -50,49 +48,39 @@ module.exports.createUser = (req, res) => {
     password,
   } = req.body;
 
-  users
-    .create({
-      name,
-      about,
-      avatar,
-      email,
-      password,
-    });
-  bcrypt.hash(req.body.password, SALT_ROUND)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res
-          .status(ERROR_CODE)
-          .send({ message: 'Ошибка обработки данных' });
-      }
-      if (!email || !password) {
-        return res
-          .status(ERROR_CODE)
-          .send({ message: 'Email или пароль не могут быть пустыми!!!' });
-      }
+  if (!email || !password) {
+    return res
+      .status(ERROR_CODE)
+      .send({ message: 'Email или пароль не могут быть пустыми' });
+  }
 
-      return res
-        .status(INTERNAL_SERVER_ERROR)
-        .send({ message: 'Ошибка по умолчанию.' });
-    });
-};
+  bcrypt.hash(password, SALT_ROUND, (error, hash) => {
+    if (error) return res.status(500).send({ message: 'errors' });
+    users
+      .findOne({ email })
+      .then((user) => {
+        if (user) {
+          return res
+            .status(403)
+            .send({ message: 'такой пользователь уже существует' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: err });
+      });
 
-module.exports.login = (req, res) => {
-  const { email, password } = req.body;
-
-  return users.findUserByCredentials(email, password)
-    .then((user) => {
-      // аутентификация успешна! пользователь в переменной user
-      const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
-      res.send({ token });
-    })
-    .catch((err) => {
-      // ошибка аутентификации
-      res
-        .status(AUTHORIZATION_REQUIRED)
-        .send({ message: err.message });
-    });
+    users
+      .create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      })
+      .then(({ user }) => {
+        res.status(201).send({ user });
+      });
+  });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
