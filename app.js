213042fc1d1783288a80f74641a50469828643ea
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-12-01 12:47:08 +0300

    users joi

--- app.js
+++ app.js
@@ -25,10 +25,15 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(2),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^((http|https|ftp):\/\/)\www.?\[a-zA-Z0-9_]#$/).min(2).max(30),
   }),
 }), createUser);
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(2),
+  }),
+}), login);
 
 // авторизация
 app.use(auth);
--- routers/users.js
+++ routers/users.js
@@ -1,4 +1,5 @@
 const userRouters = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUserMe,
@@ -10,8 +11,16 @@ const {
 
 userRouters.get('/users/me', getUserMe);
 userRouters.get('/users', getUser);
-userRouters.get('/users/:userId', getUserId);
+userRouters.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    postId: Joi.string().hex().length(24),
+  }),
+}), getUserId);
 userRouters.patch('/users/me', updateUserNameAndabout);
-userRouters.patch('/users/me/avatar', updateUserAvatar);
+userRouters.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(/^((http|https|ftp):\/\/)\www.?\[a-zA-Z0-9_]#$/).min(2).max(30),
+  }),
+}), updateUserAvatar);
 
 module.exports = userRouters;
