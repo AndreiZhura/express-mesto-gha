e7b3f87b2b741660599a23af13ecbd663905d8f6
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-12-02 13:21:22 +0300

    почти все исправил после первого ревью(errors)

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use('/', userCardsRouters);
 
 app.use('*', (req, res, next) => { next(new NotFoundError('Запрашиваемый ресурс не найден')); });
 
+app.use(errors());
+
 app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
@@ -58,7 +60,5 @@ app.use((err, req, res, next) => {
     });
   next();
 });
-
-app.use(errors());
 
 app.listen(PORT);
